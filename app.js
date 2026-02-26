5894f588a1b514d2ac97b0d2e96c8b217fdb7086
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-26 10:25:28 +0530

    api creation

--- app.js
+++ app.js
@@ -1,39 +1,70 @@
-// console.log("app file loaded..")
-// ///user file..
+//#express require
+const express = require("express"); //express module
+//create an object of express
+const app = express();
 
-// // const userName = require("./users") //users.js
-// // console.log("userName from users.js",userName)
+//API CREATION...
 
-// const user = require("./users")
-// console.log(user)//{}
+//http://localhost:3000/test
+app.get("/test", (req, res) => {
+  console.log("test api called....");
+  res.send("Test Api Called...");
+});
 
-// console.log(user.userAge)
+const user = {
+  id: 101,
+  name: "amit",
+  age: 23,
+  salary: 23000,
+};
+//http:localhost:3000/user
+app.get("/user", (req, res) => {
+  //res.json(user)
+  res.json({
+    message: "user fetched successfully!!",
+    data: user,
+  });
+});
 
-// //function call..
-// var ans = user.userData(100) //function -->users.js
-// console.log("ans =",ans)
+//dummy users array
+const users = [
+  { id: 1, name: "raj", age: 23 },
+  { id: 2, name: "parth", age: 24 },
+  { id: 3, name: "jay", age: 25 },
+];
 
-// // const userAge = require("./users")
-// // console.log("userage",userAge)
-
-// console.log("Hello From node js")
-// var x = 100
-// console.log("x = ",x)
-
-
-const employee = require("./employee")
-console.log(employee) //{}
-
-console.log(employee.empName)
-console.log(employee.empSalary)
-
-employee.empData()
+//http:localhost:3000/users
+app.get("/users",(req,res)=>{
+    res.json({
+        message:"all users",
+        data:users
+    })
+})
 
 
+//url -->data
+//http:localhost:3000/user/1234
+app.get("/user/:id",(req,res)=>{
+    //:id -->wild char --.
+    //req.params -->id
+    //req.params -->{} --> {id:..}
+    console.log(req.params)
+    console.log(req.params.id)
+
+    //task --> id -->find that id record from dummy array
+    //if record found send as response ..
+    //if user not found from id / it is not available send response message user not found with id
+    //HINT : find function
+    //Note: dont forget to send response from both if and else
+
+    res.json({
+        message:"user fetched...",
+    })
+})
 
 
-
-
-
-
-
+//server creation...
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server started on PORT ${PORT}`);
+});
--- employee.js
+++
@@ -1,9 +0,0 @@
-var empName = "amit"
-var empSalary =2000
-
-const empData = ()=>{
-    console.log("emp data called..")
-}
-module.exports ={
-    empName,empData,empSalary
-}
